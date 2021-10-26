501e4ff7f16253f73207edb14a4c306a2af832ad
Guy Blumenthal <>
2021-10-25 21:09:49 -0400

    Added file logging for CANBus in HIL testing files

--- hw-in-the-loop-tests/lib/bus.py
+++ hw-in-the-loop-tests/lib/bus.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import can
 import threading
+import os
 from typing import Callable
 from datetime import datetime
 from frame import CANFrame
@@ -15,10 +16,12 @@ def _monitor_bus(bus_obj: CANBus):
 
             # TODO: replace with python-can timestamps if
             #       this isn't goot enough
+            bus_obj._log_file.write(f"[{datetime.now()}]: {frame.to_dict}")
             bus_obj._rx_callback(frame, datetime.now())
 
 class CANBus():
     def __init__(self, interface):
+        self._log_file = open(f"hw-in-the-loop-tests/bus_logging.txt", 'a')
         self._rx_callback = lambda *args: None
         self._bus = can.ThreadSafeBus(channel = interface, bustype = _BUSTYPE)
         self._monitor_bus_thread = threading.Thread(
@@ -26,7 +29,10 @@ class CANBus():
 
         self._monitor_bus_thread.start()
 
-    def put_frame(self, frame: CANFame):
+    def __del__(self):
+        self._log_file.close()
+
+    def put_frame(self, frame: CANFrame):
         msg = can.Message(
             arbitration_id = frame.arb_id, data = frame.payload, is_extended_id = False)
         self._bus.send(msg)
