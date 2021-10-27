8c95f90515b76eccb03de44618615da472e3e99d
Guy Blumenthal <guy.blumenthal@outlook.com>
2021-10-26 20:50:23 -0400

    simple state change+

--- hw-in-the-loop-tests/simple_state_change.py
+++ hw-in-the-loop-tests/simple_state_change.py
@@ -9,22 +9,27 @@ MC_STATE_CHANGE_COMMAND = 0x15
 LV_READY_STATE = 0x01
 
 
-def on_rx(state_acks):
-    return (
-        lambda frame, timestamp:
-        print("Hi, {frame.arb_id}")
-    )
+def watch_acks_on_rx(state_acks):
+    def on_rx(frame, timestamp):
+        if frame.arb_id == BMS_STATE_CHANGE_COMMAND and frame.payload[0] == LV_READY_STATE:
+            state_acks[0] = True
+    return on_rx
 
 @click.command()
 @click.option("--interface", help = "ex. can0", required = True)
 def script_name(interface):
     with CANBus(interface) as bus:
-        bus.set_rx_callback(on_rx)
-        
+        state_acks = [False]
+        bus.set_rx_callback(watch_acks_on_rx(state_acks))
+
         frame = CANFrame(CONTROLLER_COMMAND, LV_READY_STATE)
         bus.put_frame(frame)
 
-        
+        print("Waiting for state machine", end="")
+        while not state_acks[0]:
+            pass     
+
+        print("ACK Received, transitioned from Resting -> LV Armed")
 
 
 if __name__ == "__main__":
