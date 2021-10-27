7d4663edf80e2a57798e6fb374dda993c5aed1da
Guy Blumenthal <guy.blumenthal@outlook.com>
2021-10-27 18:20:36 -0400

    Removed int cast for to_dict function in CANFrame -> Caused crashes

--- hw-in-the-loop-tests/lib/frame.py
+++ hw-in-the-loop-tests/lib/frame.py
@@ -7,7 +7,7 @@ class CANFrame():
 
     def to_dict(self):
         return {
-            "arb_id": int("{:03x}".format(self.arb_id)),
-            "payload": [int("{:02x}".format(i)) for i in self.payload]
+            "arb_id": "{:03x}".format(self.arb_id),
+            "payload": ["{:02x}".format(i) for i in self.payload]
         }
 
